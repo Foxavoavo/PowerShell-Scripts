61da7e5411ae22818c6df358ea5410f17660570a
Gus <106258439+Foxavoavo@users.noreply.github.com>
2024-01-23 10:21:12 +0000

    Update Get-MailGroupsAndShareMailboxes.ps1

--- Microsoft Exchange/Reports/Get-MailGroupsAndShareMailboxes.ps1
+++ Microsoft Exchange/Reports/Get-MailGroupsAndShareMailboxes.ps1
@@ -1,20 +1,24 @@
-#mail info
+<#
+create a list of mailgroups and shared mailbox from exchange online.
+
+*you need the PS modude for Exchange online and Exchange Administrative-level credentials.
+#>
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 #
-Remove-item 'C:\Scripts_Gus\Exchange\Results\MailgroupsANDSharedmailboxes.csv' -ea silentlycontinue
+Remove-item 'C:\Scripts_Gus\Exchange\Results\MailgroupsANDSharedmailboxes.csv' -ErrorAction silentlycontinue
 Add-Content 'C:\Scripts_Gus\Exchange\Results\MailgroupsANDSharedmailboxes.csv' "DisplayName,PrimarySmtpAddress,GroupType,ManagedBy,Description"
-$res = 'C:\Scripts_Gus\Exchange\Results\MailgroupsANDSharedmailboxes.csv'
+$results = 'C:\Scripts_Gus\Exchange\Results\MailgroupsANDSharedmailboxes.csv'
 # all mail groups in PPf online
-$mailgroupds = Get-DistributionGroup | Select-Object DisplayName, PrimarySmtpAddress, GroupType, ManagedBy, Description
-foreach ($group in $mailgroupds) {
-    $DisplayName = $group.DisplayName
-    $PrimarySmtpAddress = $group.PrimarySmtpAddress
-    $GroupType = $group.GroupType
-    $ManagedBy = $group.ManagedBy
-    $Description = $group.Description
-    Add-Content $res "$DisplayName,$PrimarySmtpAddress,$GroupType,$ManagedBy,$Description"
-    Start-Sleep -s 1 
+$MailGroups = Get-DistributionGroup | Select-Object DisplayName, PrimarySmtpAddress, GroupType, ManagedBy, Description
+foreach ($Group in $MailGroups) {
+    $DisplayName = $Group.DisplayName
+    $PrimarySmtpAddress = $Group.PrimarySmtpAddress
+    $GroupType = $Group.GroupType
+    $ManagedBy = $Group.ManagedBy
+    $Description = $Group.Description
+    Add-Content $results "$DisplayName,$PrimarySmtpAddress,$GroupType,$ManagedBy,$Description"
+    Start-Sleep -Seconds 1 
     "===>>> $DisplayName ===>>> Done!"
 }
 "Moving to distribution list now"
@@ -26,8 +30,8 @@ foreach ($shared in $sharedmailboxes) {
     $GroupType = $shared.RecipientTypeDetails
                                      
                                      
-    Add-Content $res "$DisplayName,$PrimarySmtpAddress,$GroupType"
-    Start-Sleep -s 1 
+    Add-Content $results "$DisplayName,$PrimarySmtpAddress,$GroupType"
+    Start-Sleep -Seconds 1 
     "===>>>  $DisplayName ===>>>  Done!"
 }
 "Fisnish"
