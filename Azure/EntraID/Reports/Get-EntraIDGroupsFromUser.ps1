1f8eec8ef80d1da44e9c7ee574c12de91449c602
Gus <106258439+Foxavoavo@users.noreply.github.com>
2024-01-24 16:57:23 +0000

    Create Get-EntraIDGroupsFromUser.ps1

---
+++ Azure/EntraID/Reports/Get-EntraIDGroupsFromUser.ps1
@@ -0,0 +1,32 @@
+<# get entraID user membership
+# get all user Entra ID groups and write them in a Report.
+#>
+import-module AzureAD
+Connect-AzureAD
+#
+remove-item -Path "C:\Scripts_Gus\Azure\Results\group_list.csv" -ea SilentlyContinue
+Add-Content "C:\Scripts_Gus\Azure\Results\group_list.csv" "DisplayName, MailEnabled, Mail"
+$Result = "C:\Scripts_Gus\Azure\Results\group_list.csv" 
+#
+$RAW_Users = Import-Csv 'C:\Scripts_Gus\Azure\RAW\LGUsers.csv'
+foreach ($user in $RAW_Users) {
+    $Usr = $user.Name
+    #checks
+    Start-Sleep -s 1
+    $Object = get-azureADuser -SearchString $Usr | Select-Object ObjectId | Select-Object -First 1 -ea SilentlyContinue
+    if ($Null -ne $Object) {
+        $AzureGroups = Get-AzureADUserMembership -ObjectId $Object.ObjectId | Select-Object DisplayName, Mail, MailEnabled -ea SilentlyContinue
+        foreach ($AzureGroup in $AzureGroups) {
+            $AG = $AzureGroup.DisplayName
+            $GT = $AzureGroup.MailEnabled
+            $E = $AzureGroup.Mail 
+            #report here
+            Start-Sleep -s 1
+            "===================>>>>> $AG checked okay"
+            Add-Content $Result "$AG,$GT,$e" 
+        }
+    }
+    ElseiF ($Null -eq $Object) {
+        "$Usr not in Azure"
+    }
+}
